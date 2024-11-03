a41a73cee0696338ae829b1cc98638bbe0ead8f7
Youssef Bounit <youyoussef769@hotmail.com>
2024-11-04 00:49:24 +0100

    add validation to add client

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,53 +1,111 @@
 @extends('layouts.master')
-@section('page-header')
-				<!-- breadcrumb -->
-				<div class="breadcrumb-header justify-content-between">
-					<div class="my-auto">
-						<div class="d-flex">
-							<h4 class="content-title mb-0 my-auto">Clients</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ clients list</span>
-						</div>
-					</div>
-					<div class="d-flex my-xl-auto right-content">
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-info btn-icon ml-2"><i class="mdi mdi-filter-variant"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-danger btn-icon ml-2"><i class="mdi mdi-star"></i></button>
-						</div>
-						<div class="pr-1 mb-3 mb-xl-0">
-							<button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button>
-						</div>
-						<div class="mb-3 mb-xl-0">
-							<div class="btn-group dropdown">
-								<button type="button" class="btn btn-primary">14 Aug 2019</button>
-								<button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" id="dropdownMenuDate" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="sr-only">Toggle Dropdown</span>
-								</button>
-								<div class="dropdown-menu dropdown-menu-left" aria-labelledby="dropdownMenuDate" data-x-placement="bottom-end">
-									<a class="dropdown-item" href="#">2015</a>
-									<a class="dropdown-item" href="#">2016</a>
-									<a class="dropdown-item" href="#">2017</a>
-									<a class="dropdown-item" href="#">2018</a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- breadcrumb -->
+@section('css')
+<style>
+    .clients_table #clientsTable_filter{
+        width: 100%;
+    }
+    .clients_table #clientsTable_filter input{
+        margin: 0px;
+        width: 100%;
+    }
+    .phone_select_input  .iti--allow-dropdown{
+        width: 100% !important;
+    }
+    #clientsTable{
+        width:99%;
+    }
+</style>
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+
+<!--Internal  TelephoneInput css-->
+<link rel="stylesheet" href="{{URL::asset('assets/plugins/telephoneinput/telephoneinput-rtl.css')}}">
 @endsection
+@section('page-header')
+    <!-- breadcrumb -->
+    <div class="breadcrumb-header justify-content-between">
+        <div class="my-auto">
+            <div class="d-flex">
+                <h4 class="content-title mb-0 my-auto">Clients</h4>
+                <span class="text-muted mt-1 tx-13 mr-2 mb-0">/ clients list</span>
+            </div>
+        </div>
+        <div class="d-flex my-xl-auto right-content">
+            <div class="pr-1 mb-3 mb-xl-0"  data-toggle="tooltip" data-placement="bottom" title="Add New Client">
+                <button type="button" class="modal-effect btn btn-primary btn-icon ml-2" data-effect="effect-fall" data-toggle="modal" href="#modaldemo8">
+                    <i class="mdi mdi-plus"></i>
+                </button>
+            </div>
+            <div class="pr-1 mb-3 mb-xl-0">
+                <button type="button" class="btn btn-warning btn-icon ml-2" data-toggle="tooltip" data-placement="bottom" title="Refresh" onclick="location.reload()">
+                    <i class="mdi mdi-refresh"></i>
+                </button>
+            </div>
+        </div>
+        <div class="modal" id="modaldemo8">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content modal-content-demo">
+					<div class="modal-header">
+                        <h6 class="modal-title">Add New Client</h6>
+                        <button aria-label="Close" class="close" data-dismiss="modal" type="button">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Form for Adding a New Client -->
+                        <form action="{{ route('clients.store') }}" method="POST"  data-parsley-validate="">
+                            @csrf
+                            <div class="form-group">
+                                <label for="cin">CIN <span class="tx-danger">*</span></label>
+                                <input type="text" id="cin" name="cin" placeholder="cin..." class="form-control" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="first_name">First Name <span class="tx-danger">*</span></label>
+                                <input type="text" id="first_name" name="first_name" placeholder="first name..." class="form-control" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="last_name">Last Name <span class="tx-danger">*</span></label>
+                                <input type="text" id="last_name" name="last_name" placeholder="last name..." class="form-control" required="">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" id="email" name="email" placeholder="email..." class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone <span class="tx-danger">*</span></label>
+                                <div class="input-group phone_select_input">
+                                    <input type="tel" id="phone" name="phone"  class="form-control" required="">
+								</div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn ripple btn-primary">Save changes</button>
+                                <button class="btn ripple btn-secondary" data-dismiss="modal" type="button">Close</button>
+                            </div>
+                        </form>
+                    </div>
+				</div>
+			</div>
+		</div>
+    </div>
+    <!-- breadcrumb -->
+@endsection
+
 @section('content')
 <div>
-    <div class="col-xl-12">
+    <div class="col-xl-12 clients_table">
         <div class="card mg-b-20">
             <div class="card-header pb-0">
                 <div class="d-flex justify-content-between">
-                    <h4 class="card-title mg-b-0">clients list</h4>
-                    <i class="mdi mdi-dots-horizontal text-gray"></i>
+                    <h4 class="card-title mg-b-0">Clients List</h4>
                 </div>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table id="example" class="table key-buttons text-md-nowrap">
+                    <table id="clientsTable" class="table key-buttons text-md-nowrap">
                         <thead>
                             <tr>
                                 <th class="border-bottom-0">CIN</th>
@@ -67,7 +125,7 @@
                                     <td>{{ $client->email ?? 'N/A' }}</td>
                                     <td>{{ $client->phone }}</td>
                                     <td>
-
+                                        <!-- Action buttons (e.g., edit, delete) -->
                                     </td>
                                 </tr>
                             @endforeach
@@ -81,23 +139,37 @@
 @endsection
 
 @section('js')
-<!-- Internal Data tables -->
+<!-- DataTables Scripts -->
 <script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
 <script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
-<!--Internal  Datatable js -->
 <script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+<!-- Internal Modal js-->
+<script src="{{URL::asset('assets/js/modal.js')}}"></script>
+
+<script src="{{URL::asset('assets/plugins/telephoneinput/telephoneinput.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/telephoneinput/inttelephoneinput.js')}}"></script>
+
+<!--Internal  Parsley.min js -->
+<script src="{{URL::asset('assets/plugins/parsleyjs/parsley.min.js')}}"></script>
+
+<script>
+    $(document).ready(function () {
+        $('#clientsTable').DataTable({
+            columnDefs: [
+                { orderable: false, searchable: false, targets: -1 } // Disable filter and sort for the "Actions" column
+            ],
+            language: {
+                searchPlaceholder: 'Search clients...',
+                sSearch: '',
+                lengthMenu: '_MENU_',
+            },
+        });
+        const phoneInput = document.querySelector("#phone");
+        window.intlTelInput(phoneInput, {
+            initialCountry: "ma", // Morocco
+            preferredCountries: ["ma"],
+            utilsScript: "{{URL::asset('assets/plugins/telephoneinput/utils.js')}}"
+        });
+    });
+</script>
 @endsection
